23131911ec736bacd06cea7a313bd69ade02df70
PauloSantos05 <pauloprogramer123@outlook.com>
2025-10-06 21:30:58 -0300

    Learned about MVC(Model View Controller) and added a code example

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    public function index()
+    {
+        $chirps = [
+            ['id' => 1, 'user' => 'Alice', 'message' => 'Hello, world!'],
+            ['id' => 2, 'user' => 'Bob', 'message' => 'Laravel is awesome!'],
+            ['id' => 3, 'user' => 'Charlie', 'message' => 'Just chirping around.'],
+        ];
+
+        return view('home', ['chirps' => $chirps]);
+    }
+}
